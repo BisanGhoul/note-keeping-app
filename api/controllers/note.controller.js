61604a32c3ae6aece3b6fafa70afc7c8728cc3ba
Bisan <bisan.elghoul@gmail.com>
2025-03-28 22:00:01 +0200

    feat: Add validation for Note ID and fetch single note by ID
    - Implemented ObjectId validation for noteID in getNoteByID controller
    - Added response for invalid Note ID format and missing records
    - Updated route to handle fetching a single note by ID

--- api/controllers/note.controller.js
+++ api/controllers/note.controller.js
@@ -23,3 +23,35 @@ export const getAllNotes = async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 };
+
+// Get a single note by ID
+export const getNoteByID = async (req, res, next) => {
+    try {
+        const { noteID } = req.params;
+
+        if (!mongoose.Types.ObjectId.isValid(noteID)) {
+            return res.status(400).json({
+                error: "Invalid Note Id format",
+            });
+        }
+
+        const note = await Note.findById(noteID).select(
+            "title content createdAt _id"
+        );
+
+        if (!note) {
+            return res.status(404).json({ message: "No record with this ID." });
+        }
+
+        res.status(200).json({
+            note,
+            request: {
+                type: "GET",
+                url: `http://localhost:3000/notes`,
+            },
+        });
+    } catch (error) {
+        console.error("Error fetching note:", error);
+        res.status(500).json({ error: error.message });
+    }
+};
--- api/routes/notes.js
+++ api/routes/notes.js
@@ -1,7 +1,8 @@
 import express from "express";
-import { getAllNotes } from "../controllers/note.controller.js";
+import { getAllNotes, getNoteByID } from "../controllers/note.controller.js";
 const router = express.Router();
 
 router.get("/", getAllNotes);
+router.get("/:noteID", getNoteByID);
 
 export default router;
